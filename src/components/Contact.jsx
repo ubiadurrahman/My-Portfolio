351fd99edf772ecdeb2fa30a8934ceba6bb6c16b
Ubaid ur Rahman <ubaidurrahman.dev@email.com>
2025-05-31 06:27:33 +0500

    Replacing the Email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ export default function Contact() {
               >
                 <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
-              <div className="ml-4 text-sm sm:text-base">ur83420@gmail.com</div>
+              <div className="ml-4 text-sm sm:text-base">ubiadurrahman.dev@gmail.com</div>
             </div>
           </div>
 
